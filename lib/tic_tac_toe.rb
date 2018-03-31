c13fe423f22380c5b6739a3b1cd9290b73fb9b96
Matt Farmer <mrfarmer777@gmail.com>
2018-03-31 14:58:42 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -36,12 +36,13 @@ def turn(board)
 end
 
 def turn_count(board)
-
-  turns = board.each do |pos|
+  count=0
+  board.each do |pos|
     if(pos=="X"||pos=="O")
       count+=1
     end
   end
+  count
 end
 
 def current_player(board)
